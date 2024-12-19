cf4d5d4f38cee52339cffd96400d9c0b6ce910ae
antisdun <112616257+antisdun@users.noreply.github.com>
2024-12-19 17:10:44 +0900

    [markup] SelectOption 컴포넌트 제작 - SelectOption 컴포넌트 및 스토리북 제작 [markup] SelectOption 컴포넌트 #4

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -4,3 +4,4 @@ export { default as Button } from './Button/Button';
 export { default as Input } from './Input/Input';
 export { default as Label } from './Label/Label';
 export { default as Icon } from './Icon/Icon';
+export { default as SelectOption } from './SelectOption/SelectOption';
