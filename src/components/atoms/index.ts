5e1c79b02a2747f729cb35388515556e9c47ebe9
elitezer0 <diana.yy.jeong@gmail.com>
2024-12-21 09:13:31 +0900

    Reapply "[markup] TeamTag 컴포넌트 제작"
    This reverts commit d69a1fbcb6635e9073dc0639727f19af38fd278d.

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -8,3 +8,4 @@ export { default as SelectOption } from './SelectOption/SelectOption';
 export { default as CalendarTag } from './CalendarTag/CalendarTag';
 export { default as TabButton } from './TabButton/TabButton';
 export { default as ButtonPDF } from './ButtonPDF/ButtonPDF';
+export { default as TeamTag } from './TeamTag/TeamTag';
