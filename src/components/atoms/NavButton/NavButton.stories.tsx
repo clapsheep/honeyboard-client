c2e8ed5722968df0cb88595efaad37e512d51253
jump6746 <jump6746@naver.com>
2024-12-24 10:01:25 +0900

    [markup] : NavButton 컴포넌트 및 스토리북 제작 - props에 Link 태그를 위한 link 추가했습니다. - hasSub 유무로 Link 태그가 되거나 Button 태그가 됩니다. - 아직 items는 연결하지 않았습니다.
    -

--- src/components/atoms/NavButton/NavButton.stories.tsx
+++ src/components/atoms/NavButton/NavButton.stories.tsx
@@ -1,6 +1,7 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryFn, StoryObj } from '@storybook/react';
 import NavButton from './NavButton';
 import Icon from '../Icon/Icon';
+import { BrowserRouter } from 'react-router';
 
 const meta = {
     title: 'Components/Atoms/NavButton',
@@ -9,21 +10,17 @@ const meta = {
         layout: 'centered',
     },
     tags: ['autodocs'],
+    decorators: [
+        (Story: StoryFn) => (
+            <BrowserRouter>
+                <Story />
+            </BrowserRouter>
+        ),
+    ],
 } satisfies Meta<typeof NavButton>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
-
-export const HasSub: Story = {
-    args: {
-        id: '1',
-        title: '메인 페이지',
-        icon: <Icon id="calendar" />,
-        hasSub: true,
-        isActive: false,
-        onClick: () => {},
-    },
-};
 
 export const NoSub: Story = {
     args: {
@@ -31,6 +28,20 @@ export const NoSub: Story = {
         title: '메인 페이지',
         icon: <Icon id="calendar" />,
         hasSub: false,
+        isActive: false,
+        link: '/main',
+        onClick: () => {
+            alert('하위 메뉴 열기!');
+        },
+    },
+};
+
+export const HasSub: Story = {
+    args: {
+        id: '1',
+        title: '메인 페이지',
+        icon: <Icon id="calendar" />,
+        hasSub: true,
         isActive: false,
         onClick: () => {},
     },
--- src/components/atoms/NavButton/NavButton.tsx
+++ src/components/atoms/NavButton/NavButton.tsx
@@ -1,13 +1,14 @@
+import { Link } from 'react-router';
 import Icon from '../Icon/Icon';
-
 interface NavButtonProps {
     id: string;
     title: string;
     icon?: React.ReactNode;
     hasSub?: boolean;
+    link?: string;
     // items: SubNavButtonProps[];
     isActive?: boolean;
-    onClick: (e: React.MouseEvent) => void;
+    onClick?: (e: React.MouseEvent) => void;
 }
 
 const NavButton = ({
@@ -15,49 +16,49 @@ const NavButton = ({
     title,
     icon = null,
     hasSub = false,
+    link = '/',
     // items,
     isActive = false,
     onClick,
 }: NavButtonProps) => {
+    const Component = hasSub ? 'button' : Link;
+
     const COLOR_PROS = {
         true: 'bg-blue-100 border border-gray-200',
         false: 'bg-gray-25 border border-gray-200',
     };
 
-    const hasSubContentColor = isActive
+    const HAS_SUB_CONTENT_COLOR = isActive
         ? 'border-2 border-bluegray-400 bg-gray-25'
         : 'border border-gray-200 bg-gray-25';
 
-    const noSubContentColor = isActive
+    const NO_SUB_CONTENT_COLOR = isActive
         ? COLOR_PROS['true']
         : COLOR_PROS['false'];
 
-    const hasSubContent = (
+    const HAS_SUB_CONTENT = (
         <Icon
             id="down"
             className={`ml-auto mr-6 transition-transform duration-500 ${isActive ? 'rotate-180' : ''}`}
         />
     );
 
-    const noSubContent = isActive && (
-        <div className="ml-auto h-full w-[0.375rem] bg-blue-700"></div>
-    );
+    const NO_SUB_CONTENT = isActive && 'border-r-[0.375rem] border-blue-700';
 
     return (
         <li
             key={id}
-            className={`box-border list-none hover:bg-bluegray-100 ${hasSub ? hasSubContentColor : noSubContentColor}`}
+            className={`box-border list-none hover:bg-bluegray-100 ${hasSub ? HAS_SUB_CONTENT_COLOR : NO_SUB_CONTENT_COLOR}`}
         >
-            <button
-                type="button"
+            <Component
+                to={link}
                 onClick={onClick}
-                className="flex h-10 w-60 items-center gap-1 pl-9"
-                aria-expanded={isActive}
+                className={`flex h-10 w-60 items-center gap-1 pl-9 ${hasSub ? '' : NO_SUB_CONTENT}`}
             >
                 {icon}
                 <span className="font-semibold text-gray-900">{title}</span>
-                {hasSub ? hasSubContent : noSubContent}
-            </button>
+                {hasSub ? HAS_SUB_CONTENT : null}
+            </Component>
         </li>
     );
 };
