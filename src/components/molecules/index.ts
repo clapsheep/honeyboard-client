80bf498357a1dc562aae29c841e9574a61df397d
antisdun <112616257+antisdun@users.noreply.github.com>
2024-12-24 21:36:29 +0900

    [markup] MusicTrack 컴포넌트 제작 - MusicTrack 컴포넌트 및 스토리북 제작 [markup] MusicTrack 컴포넌트 #37

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,2 +1,3 @@
 export { default as WebSiteCard } from './WebSiteCard/WebSiteCard';
 export { default as AlgoDetailCard } from './AlgoDetailCard/AlgoDetailCard';
+export { default as MusicTrack } from './MusicTrack/MusicTrack';
