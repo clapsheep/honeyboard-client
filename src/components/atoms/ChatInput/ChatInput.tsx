1a3f7a59b765ac459cc1a9c66d177e8da1a2f22b
antisdun <112616257+antisdun@users.noreply.github.com>
2024-12-26 22:29:41 +0900

    [refactor] ChatInput 컴포넌트 리팩토링 - input 태그를 textarea로 변경 [markup] ChatInput 컴포넌트 #36

--- src/components/atoms/ChatInput/ChatInput.tsx
+++ src/components/atoms/ChatInput/ChatInput.tsx
@@ -3,25 +3,34 @@ import Icon from '../Icon/Icon';
 interface ChatInputProps {
     id: string;
     value: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    textareaRef: React.RefObject<HTMLTextAreaElement>;
+    onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
     onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const ChatInput = ({ id, value, onChange, onClick }: ChatInputProps) => {
+const ChatInput = ({
+    id,
+    value,
+    textareaRef,
+    onChange,
+    onClick,
+}: ChatInputProps) => {
     return (
         <div className="relative flex items-center">
-            <input
+            <textarea
                 id={id}
                 name={id}
                 value={value}
                 placeholder="채팅을 입력하세요"
+                ref={textareaRef}
+                rows={1}
                 onChange={onChange}
-                className="w-full rounded-2xl border border-gray-400 px-5 py-4 pr-14 shadow-md placeholder:text-gray-500"
+                className="w-full resize-none overflow-hidden rounded-2xl border border-gray-400 px-5 py-4 pr-14 shadow-md placeholder:text-gray-500"
             />
             <button
                 type="button"
                 onClick={onClick}
-                className="absolute right-5"
+                className="absolute bottom-4 right-5"
             >
                 <Icon id="send" size={24}></Icon>
             </button>
