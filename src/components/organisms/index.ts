371d5713332507dd3a8bd1cf3daa96f9484160c9
clapsheep <sktndid1203@naver.com>
2024-12-31 18:21:48 +0900

    [feat] 웹 개념, 웹 추천 리스트 페이지 마크업 및 기능 구현 - Suspense를 사용한 로딩 중 스켈레톤 UI 표시 - 서치바, 북마크 미구현 - 카드 컴포넌트 스토리북 오류 아직 미해결

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -3,3 +3,4 @@ export { default as EmailVerificationModal } from './EmailVerificationModal/Emai
 export { default as Navigation } from './Navigation/Navigation';
 export { default as BasicModal } from './BasicModal/BasicModal';
 export { default as Header } from './Header/Header';
+export { default as ProjectCardSkeleton } from './ProjectCard/ProjectCardSkeleton';
