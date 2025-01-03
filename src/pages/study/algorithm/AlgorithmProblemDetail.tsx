fd52eb9d032473db1649851538f816d3869640c1
elitezer0 <diana.yy.jeong@gmail.com>
2025-01-04 02:08:34 +0900

    [feat] AlgoInfo 컴포넌트 설정

--- src/pages/study/algorithm/AlgorithmProblemDetail.tsx
+++ src/pages/study/algorithm/AlgorithmProblemDetail.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/atoms';
 import { Header } from '@/components/organisms';
 import { useContentDetail } from '@/hooks/useContentDetail';
 import { useLocation, useParams } from 'react-router';
+import AlgoInfo from '@/components/molecules/AlgoInfo/AlgoInfo';
 import ToastViewerComponent from '@/layouts/ToastViewerComponent';
 import {
     getAlgorithmSolutionAPI,
@@ -32,7 +33,12 @@ const AlgorithmProblemDetail = () => {
                     }}
                     BreadcrumbProps={{ pathname }}
                 >
-                    <div className="flex justify-end">
+                    <div className="flex items-start justify-between">
+                        <AlgoInfo
+                            memory={data.memory}
+                            runtime={data.runtime}
+                            languageId={data.languageId}
+                        ></AlgoInfo>
                         <div className="flex gap-4">
                             <Button color="red" onClick={handleDelete}>
                                 풀이 삭제
