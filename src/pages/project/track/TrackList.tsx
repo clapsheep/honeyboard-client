8952a105ae09046e2076703a6e1f9d18e9ee1b16
SungMoonPark <taim831@naver.com>
2025-01-04 00:15:13 +0900

    [markup] 관통 프로젝트 리스트 마크업

--- src/pages/project/track/TrackList.tsx
+++ src/pages/project/track/TrackList.tsx
@@ -1,8 +1,8 @@
-import { Button, SelectOption } from '@/components/atoms';
-import { TabNavigation } from '@/components/molecules';
-import { Header } from '@/components/organisms';
-import { useAuth } from '@/hooks/useAuth';
+import { Button, NameTag, SelectOption } from '@/components/atoms';
+import { TabNavigation, TeamTag } from '@/components/molecules';
+import { Header, ProjectCard } from '@/components/organisms';
 import { useGenerationStore } from '@/stores/generationStore';
+import { useUserStore } from '@/stores/userStore';
 import { convertSelectType } from '@/utils/convertSelectType';
 import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router';
@@ -10,21 +10,58 @@ import { useLocation, useNavigate } from 'react-router';
 const TrackList = () => {
     const navigate = useNavigate();
     const { pathname } = useLocation();
-    const { userInfo } = useAuth();
+    const { userInfo } = useUserStore();
     const { generationList } = useGenerationStore();
     const [generation, setGeneration] = useState<string>(
         userInfo?.generationId ?? '',
     );
     const ROUTES = [{ path: '/track', name: '프로젝트', isActive: true }];
+
+    const GENERATION_OPTIONS = [
+        { value: '13', label: '13기' },
+        { value: '12', label: '12기' },
+        { value: '11', label: '11기' },
+    ];
+    const ProjectList = [
+        {
+            title: '무슨무슨 서비스',
+            createAt: '2025-01-04',
+            id: '1',
+        },
+        {
+            title: '무슨무슨 서비스2',
+            createAt: '2025-01-04',
+            id: '2',
+        },
+        {
+            title: '무슨무슨 서비스3',
+            createAt: '2024-12-05',
+            id: '3',
+        },
+        {
+            title: '무슨무슨 서비스4',
+            createAt: '2025-01-01',
+            id: '4',
+        },
+        {
+            title: '무슨무슨 서비스4',
+            createAt: '2025-01-04',
+            id: '5',
+        },
+    ];
     return (
-        <>
+        <div>
             <Header
-                titleProps={{ title: '관통 프로젝트' }}
+                titleProps={{
+                    title: '관통 프로젝트',
+                }}
                 BreadcrumbProps={{ pathname }}
             >
-                <div className="flex items-end justify-between">
-                    <TabNavigation routes={ROUTES} />
-                    <div className="flex gap-4">
+                <div className="flex justify-between">
+                    <div className="pt-6">
+                        <TabNavigation routes={ROUTES} />
+                    </div>
+                    <div className="flex items-end gap-4">
                         {userInfo?.role === 'ADMIN' ? (
                             <Button
                                 onClick={() => {
@@ -47,7 +84,19 @@ const TrackList = () => {
                     </div>
                 </div>
             </Header>
-        </>
+            <section className="grid w-full grid-cols-1 gap-6 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {/* 프로젝트 목록 렌더링 */}
+                {ProjectList &&
+                    ProjectList.map((item, index) => (
+                        <ProjectCard
+                            key={index}
+                            id={item.id}
+                            title={item.title}
+                            subTitle={item.createAt}
+                        />
+                    ))}
+            </section>
+        </div>
     );
 };
 
