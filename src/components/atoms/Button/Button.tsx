a6eee232e027c1ff5596500f3c0a8f4744275e78
SungMoonPark <taim831@naver.com>
2025-01-03 22:34:47 +0900

    Revert "[markup] TrackList 컴포넌트 마크업"
    This reverts commit 449d1fd7ad4e70ddbe6e09ac9e473dfab471c48e.

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -2,7 +2,7 @@ interface ButtonProps {
     onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
     children: React.ReactNode;
     type?: 'button' | 'submit' | 'reset';
-    color?: 'red' | 'blue' | 'green' | 'gray' | 'white';
+    color?: 'red' | 'blue' | 'green' | 'gray';
     disabled?: boolean;
     className?: string;
     'data-id'?: string;
@@ -24,7 +24,6 @@ const Button = ({
         blue: 'bg-blue-700',
         green: 'bg-success-500',
         gray: 'bg-gray-300',
-        white: 'bg-white',
     };
 
     return (
@@ -32,7 +31,7 @@ const Button = ({
             onClick={onClick}
             type={type}
             disabled={disabled}
-            className={`rounded-sm px-4 py-1 text-text-md font-semibold ${color === 'white' ? 'border border-gray-600 text-gray-900' : 'text-gray-25'} ${COLOR_PROS[color]} ${className}`}
+            className={`rounded-sm px-4 py-1 text-text-md font-semibold text-gray-25 ${COLOR_PROS[color]} ${className}`}
         >
             {children}
         </button>
