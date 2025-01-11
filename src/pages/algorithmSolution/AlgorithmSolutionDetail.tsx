f10f219203038a8f82f26e19da994981230bf411
elitezer0 <diana.yy.jeong@gmail.com>
2025-01-11 18:08:45 +0900

    [refactor] AlgorithmProblemDetail 컴포넌트 API 인터페이스 개선
    - API 호출 매개변수 구조를 객체 형태로 변경
    - useContentDetail 훅의 타입 정의에 맞게 API 인터페이스 수정
    - 컴포넌트 이름을 AlgorithmSolutionDetail로 변경하여 목적에 맞게 수정

--- src/pages/algorithmSolution/AlgorithmSolutionDetail.tsx
+++ src/pages/algorithmSolution/AlgorithmSolutionDetail.tsx
@@ -1,5 +1,61 @@
+import { Button } from "@/components/atoms";
+import { Header } from "@/components/organisms";
+import { useContentDetail } from "@/hooks/useContentDetail";
+import { useLocation, useParams } from "react-router";
+import { AlgoInfo } from "@/components/molecules";
+import ToastViewerComponent from "@/layouts/ToastViewerComponent";
+import { getAlgorithmSolutionDetailAPI, deleteAlgorithmSolutionAPI } from "@/api/AlgorithmSolutionAPI";
+
 const AlgorithmSolutionDetail = () => {
-    return <div>알고리즘 문제 상세</div>;
-};
+    const { pathname } = useLocation();
+    const { problemId, solutionId } = useParams();
+
+    const { data, handleDelete, handleEdit, handleLike} = useContentDetail({
+        contentType: 'algo_solution',
+        contentId: solutionId!,
+        getDetailAPI: ({ guideId }) => getAlgorithmSolutionDetailAPI({ 
+            problemId: problemId!, 
+            solutionId: guideId 
+        }),
+        deleteAPI: ({ guideId }) => deleteAlgorithmSolutionAPI({ 
+            problemId: problemId!, 
+            solutionId: guideId 
+        }),
+        navigateAfterDelete: `/study/algorithm/problem/${problemId}/solution`
+    })
+    
+    if (!data) return null;
+        return (
+            <>
+                <Header
+                    titleProps={{
+                        title: data.title,
+                        description: {'풀이 설명': data.summary},
+                        author: data.authorName,
+                        onClickLike: handleLike,
+                    }}
+                    BreadcrumbProps={{ pathname }}
+                >
+                    <div className="flex items-start justify-between">
+                        <AlgoInfo
+                            memory={data.memory}
+                            runtime={data.runtime}
+                            languageId={data.languageName}
+                        ></AlgoInfo>
+                        <div className="flex gap-4">
+                            <Button color="red" onClick={handleDelete}>
+                                풀이 삭제
+                            </Button>
+                            <Button onClick={handleEdit}>풀이 수정</Button>
+                        </div>
+                    </div>
+                </Header>
+                <ToastViewerComponent
+                        content={data.content}
+                        viewerId="viewer"
+                />
+            </>
+        );
+    };
 
 export default AlgorithmSolutionDetail;
