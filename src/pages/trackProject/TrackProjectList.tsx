86f6bb020a68ce635b35f2163757da1bb61340f4
SungMoonPark <taim831@naver.com>
2025-01-11 18:32:47 +0900

    [markup] 관통 프로젝트 리스트 페이지 마크업

--- src/pages/trackProject/TrackProjectList.tsx
+++ src/pages/trackProject/TrackProjectList.tsx
@@ -1,5 +1,93 @@
+import { Button, SelectOption } from '@/components/atoms';
+import { TabNavigation } from '@/components/molecules';
+import { Header, ProjectCard } from '@/components/organisms';
+import { useAuth } from '@/hooks/useAuth';
+import { useGenerationStore } from '@/stores/generationStore';
+import { convertSelectType } from '@/utils/convertSelectType';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+
 const TrackProjectList = () => {
-    return <div>트랙 프로젝트 목록</div>;
+    const { userInfo } = useAuth();
+    const { generationList } = useGenerationStore();
+    const navigate = useNavigate();
+    const [generationId, setGenerationId] = useState<string | null>(
+        userInfo!.generationId,
+    );
+
+    const ProjectList = [
+        {
+            title: '무슨무슨 서비스',
+            createAt: '2025-01-04',
+            id: '1',
+        },
+        {
+            title: '무슨무슨 서비스2',
+            createAt: '2025-01-04',
+            id: '2',
+        },
+        {
+            title: '무슨무슨 서비스3',
+            createAt: '2024-12-05',
+            id: '3',
+        },
+        {
+            title: '무슨무슨 서비스4',
+            createAt: '2025-01-01',
+            id: '4',
+        },
+        {
+            title: '무슨무슨 서비스4',
+            createAt: '2025-01-04',
+            id: '5',
+        },
+    ];
+    const ROUTES = [
+        { path: 'project/track', name: '프로젝트', isActive: true },
+    ];
+    return (
+        <div>
+            <Header titleProps={{ title: '관통프로젝트' }}>
+                <div className="flex justify-between">
+                    <div className="pt-6">
+                        <TabNavigation routes={ROUTES} />
+                    </div>
+                    <div className="flex items-end gap-4">
+                        {userInfo?.role === 'ADMIN' ? (
+                            <Button
+                                onClick={() => {
+                                    alert('create');
+                                }}
+                            >
+                                프로젝트 생성
+                            </Button>
+                        ) : null}
+                        <SelectOption
+                            id="generation"
+                            name="generation"
+                            placeholder="기수"
+                            options={convertSelectType(generationList)}
+                            defaultValue={generationId}
+                            onChange={(e) => {
+                                setGenerationId(e.target.value);
+                            }}
+                        />
+                    </div>
+                </div>
+            </Header>
+            <section className="grid w-full grid-cols-1 gap-6 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {ProjectList &&
+                    ProjectList.map((item, index) => (
+                        <ProjectCard
+                            key={index}
+                            id={item.id}
+                            title={item.title}
+                            subTitle={item.createAt}
+                        />
+                    ))}
+            </section>
+        </div>
+    );
 };
 
 export default TrackProjectList;
