ae50240369c0508b793dc9c005e48c409a76b07a
yiyoung yoon <2020yyoung@gmail.com>
2025-01-15 17:43:09 +0900

    feat: 알고리즘 문제 풀이 리스트 페이지 필터링 컴포넌트 마크업

---
+++ src/components/molecules/FilterChip/FilterChip.tsx
@@ -0,0 +1,64 @@
+import { CalendarTag } from "@/components/atoms";
+
+interface Option {
+    value: string;
+    label: string;
+}
+
+interface OptionGroup {
+    category: string; 
+    options: Option[];
+}
+
+interface FilterChipProps {
+    optionGroups: OptionGroup[]; 
+    selectedValues: Record<string, string[]>; 
+    onClickOption: (
+        category: string,
+        value: string,
+    ) => void; 
+    generationId: string | null; 
+    onGenerationClick: () => void; 
+}
+
+
+const FilterChip = (
+    {optionGroups,
+    selectedValues,
+    onClickOption,
+    generationId,
+    onGenerationClick}: FilterChipProps) => {
+    return (
+    <section className="flex gap-3">
+    {optionGroups.map((group) => (
+        <div key={group.category} className="flex gap-3">
+                {group.options.map((option) => (
+                    <CalendarTag
+                        key={option.value}
+                        color={
+                            selectedValues[group.category]?.includes(option.value)
+                                ? 'blue'
+                                : 'regular'
+                        }
+                        onClick={() => onClickOption(group.category, option.value)}
+                    >
+                        {option.label}
+                    </CalendarTag>
+                ))}
+        </div>
+    ))}
+   
+
+<CalendarTag
+color={generationId ? 'blue' : 'regular'}
+onClick={onGenerationClick}
+>
+                내 기수만 보기
+            </CalendarTag>
+      
+    
+</section>
+);
+}
+
+export default FilterChip;
