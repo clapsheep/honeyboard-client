2c5b720dce731e89a1df1ef8d4d3fe2792d0b56f
elitezer0 <diana.yy.jeong@gmail.com>
2025-01-23 02:50:33 +0900

    [fix] 목록 페이지네이션 동작 수정
    - 페이지 이동 단위를 5페이지로 변경

--- src/components/templates/AlgoDetailCards.tsx
+++ src/components/templates/AlgoDetailCards.tsx
@@ -136,13 +136,13 @@ const AlgoDetailCards = ({ problemId }: AlgoDetailCardsProps) => {
                             total={data.pageInfo.totalPages}
                             now={page}
                             onClickLeft={() =>
-                                handlePageChange(Math.max(1, page - 1))
+                                handlePageChange(Math.max(1, page - 5))
                             }
                             onClickRight={() =>
                                 handlePageChange(
                                     Math.min(
                                         data.pageInfo.totalPages,
-                                        page + 1,
+                                        page + 5,
                                     ),
                                 )
                             }
