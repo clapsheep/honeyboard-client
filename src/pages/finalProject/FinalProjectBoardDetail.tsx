d26dc779e9bcc4e30cddfbd082d658d4f494545f
elitezer0 <diana.yy.jeong@gmail.com>
2025-01-25 23:58:31 +0900

    [fix] 파이널 프로젝트 게시글 상세 페이지 충돌 해결 및 코드 정리

--- src/pages/finalProject/FinalProjectBoardDetail.tsx
+++ src/pages/finalProject/FinalProjectBoardDetail.tsx
@@ -1,18 +1,18 @@
 import {
     deleteFinaleProjectBoardAPI,
-    getFinaleProjectBoardDetailAPI
+    getFinaleProjectBoardDetailAPI,
 } from '@/api/finaleAPI';
 import { Button, ButtonPDF, NameTag } from '@/components/atoms';
 import { Header } from '@/components/organisms';
+import FinalPDF from '@/components/templates/PDF/FinalPDF';
 import { useAuth } from '@/hooks/useAuth';
 import { useProjectBoardDetail } from '@/hooks/useProjectBoardDetail';
 import ToastViewerComponent from '@/layouts/ToastViewerComponent';
 import { FinaleTeamMember } from '@/types/FinaleProject';
+import convertDate from '@/utils/convertDate';
 import { useLocation, useParams } from 'react-router';
 
-//finale
 const FinalProjectBoardDetail = () => {
-
     const { pathname } = useLocation();
     const { finaleProjectId, boardId } = useParams();
     const { userInfo } = useAuth();
@@ -31,55 +31,54 @@ const FinalProjectBoardDetail = () => {
             navigateAfterDelete: `/project/final/${finaleProjectId}`
         });
 
-    const handleDownloadPDF = () => {
-        alert('PDF 다운로드');
-    };
-
     if (!data) return null;
 
     return (
         <>
             <Header
-            titleProps={{
-                title: data.title,
-                description: {'요약': data.summary},
-            }}
-            BreadcrumbProps={{ pathname }}
-        >
-            <div className="flex justify-between">
-                <div className="flex gap-3">
-                    {data.members.map(
-                        (
-                            member: Pick<
-                                FinaleTeamMember,
-                                'id' | 'name' | 'role'
-                            >,
-                        ) => (
-                            <NameTag key={member.id} isLeader={member.role}>
-                                {member.name}
-                            </NameTag>
-                        ),
+                titleProps={{
+                    title: data.title,
+                    description: { 요약: data.summary },
+                }}
+                BreadcrumbProps={{ pathname }}
+            >
+                <div className="flex justify-between">
+                    <div className="flex gap-3">
+                        {data.members.map(
+                            (
+                                member: Pick<
+                                    FinaleTeamMember,
+                                    'id' | 'name' | 'role'
+                                >,
+                            ) => (
+                                <NameTag key={member.id} isLeader={member.role}>
+                                    {member.name}
+                                </NameTag>
+                            ),
+                        )}
+                    </div>
+                    {(userInfo?.role === 'ADMIN' || data.members.some((member)=> member.id===userId)) && (
+                        <div className="flex gap-4">
+                            <Button onClick={handleDelete} color="red">
+                                일지 삭제
+                            </Button>
+                            <Button onClick={handleEdit}>일지 수정</Button>
+                        </div>
                     )}
                 </div>
-                {(userInfo?.role === 'ADMIN' || data.members.some((member)=> member.id===userId)) && (
-                    <div className="flex gap-4">
-                        <Button onClick={handleDelete} color="red">
-                            일지 삭제
-                        </Button>
-                        <Button onClick={handleEdit}>일지 수정</Button>
-                    </div>
-                )}
+            </Header>
+            <div className="flex flex-col p-6">
+                <div className="flex w-full justify-end">
+                    <ButtonPDF
+                        document={<FinalPDF data={data} />}
+                        fileName={`[FINAL_${convertDate(data.createdAt)}]${data.members.map((i) => i.name).join(',')}_${data.title}.pdf`}
+                    />
+                </div>
+                <ToastViewerComponent
+                    content={data.content}
+                    viewerId="finalViewer"
+                />
             </div>
-        </Header>
-        <div className="flex flex-col p-6">
-            <div className="flex w-full justify-end">
-                <ButtonPDF onClick={handleDownloadPDF}></ButtonPDF>
-            </div>
-            <ToastViewerComponent
-                content={data.content}
-                viewerId="finalViewer"
-            />
-        </div>
         </>
     );
 };
