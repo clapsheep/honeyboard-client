8fa25342eecf37acdbb52ff26397caa43e9436c2
SungMoonPark <taim831@naver.com>
2025-02-01 23:11:47 +0900

    refactor : 에러 방지를 위한 section코드 추가

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -31,12 +31,14 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
         ref,
     ) => {
         return (
-            <div className="relative w-full">
-                {iconId && (
-                    <section className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-600">
-                        <Icon id={iconId} size={20} aria-hidden="true" />
-                    </section>
-                )}
+            <section className="relative w-full">
+                <section>
+                    {iconId && (
+                        <div className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-600">
+                            <Icon id={iconId} size={20} aria-hidden="true" />
+                        </div>
+                    )}
+                </section>
 
                 <input
                     ref={ref}
@@ -58,7 +60,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
                     placeholder={placeholder}
                     disabled={disabled}
                 />
-            </div>
+            </section>
         );
     },
 );
